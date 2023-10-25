85fad6b3f07fceb460900e9441493ed632bd0df7
myhowaibot <141584790+myhowaibot@users.noreply.github.com>
2023-10-25 20:03:04 +0330

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -26,6 +26,9 @@ read -p "Enter your ip: " VIP
 
 
 
+read -p "Enter your PR: " PR
+
+
 
 echo "making a check file"
 
@@ -56,14 +59,14 @@ configs=""
 
 for (( i=1; i<=$nm_servers; i++ ))
 do
-    read -p "Enter the ip addres of ha node$i: " MIP
+    read -p "Enter the ip addres of keepalived node$i: " MIP
     configs+="        $MIP\n"
 done
 
 
-echo -e "vrrp_script check_apiserver {\n  script '/etc/keepalived/check_apiserver.sh'\n  interval 1\n  timeout 1\n  fall 4\n  rise 2\n  weight -2\n}\n\nvrrp_instance VI_1 {\n    state BACKUP\n    interface $NET\n    virtual_router_id 60\n    priority 100\n    advert_int 3\n    unicast_src_ip $VIP\n    unicast_peer {\n$configs    }\n    authentication {\n        auth_type PASS\n        auth_pass liesdfged\n    }\n    virtual_ipaddress {\n        $IP/24\n    }\n    track_script {\n        check_apiserver\n    }\n}\n"
+echo -e "vrrp_script check_apiserver {\n  script '/etc/keepalived/check_apiserver.sh'\n  interval 1\n  timeout 1\n  fall 4\n  rise 2\n  weight -2\n}\n\nvrrp_instance VI_1 {\n    state BACKUP\n    interface $NET\n    virtual_router_id 60\n    priority $PR\n    advert_int 3\n    unicast_src_ip $VIP\n    unicast_peer {\n$configs    }\n    authentication {\n        auth_type PASS\n        auth_pass liesdfged\n    }\n    virtual_ipaddress {\n        $IP/24\n    }\n    track_script {\n        check_apiserver\n    }\n}\n"
 if yrn; then
-    echo -e "vrrp_script check_apiserver {\n  script '/etc/keepalived/check_apiserver.sh'\n  interval 1\n  timeout 1\n  fall 4\n  rise 2\n  weight -2\n}\n\nvrrp_instance VI_1 {\n    state BACKUP\n    interface $NET\n    virtual_router_id 60\n    priority 100\n    advert_int 3\n    unicast_src_ip $VIP\n    unicast_peer {\n$configs    }\n    authentication {\n        auth_type PASS\n        auth_pass liesdfged\n    }\n    virtual_ipaddress {\n        $IP/24\n    }\n    track_script {\n        check_apiserver\n    }\n}\n"  > /etc/keepalived/keepalived.conf;
+    echo -e "vrrp_script check_apiserver {\n  script '/etc/keepalived/check_apiserver.sh'\n  interval 1\n  timeout 1\n  fall 4\n  rise 2\n  weight -2\n}\n\nvrrp_instance VI_1 {\n    state BACKUP\n    interface $NET\n    virtual_router_id 60\n    priority $PR\n    advert_int 3\n    unicast_src_ip $VIP\n    unicast_peer {\n$configs    }\n    authentication {\n        auth_type PASS\n        auth_pass liesdfged\n    }\n    virtual_ipaddress {\n        $IP/24\n    }\n    track_script {\n        check_apiserver\n    }\n}\n"  > /etc/keepalived/keepalived.conf;
     sudo systemctl enable --now keepalived && jurnalctl -flu keepalived
 else
     echo "bye";
