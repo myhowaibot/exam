2b86e4dd670ae7c027d1379de9335a07cf3132ef
myhowaibot <141584790+myhowaibot@users.noreply.github.com>
2023-10-25 20:43:40 +0330

    Update ha.sh

--- ha.sh
+++ ha.sh
@@ -13,10 +13,7 @@ yrn() {
 
 
 sudo apt install --no-install-recommends software-properties-common -y
-
-sudo add-apt-repository ppa:vbernat/haproxy-2.4 -y
-
-sudo apt install haproxy=2.4 -y 
+sudo apt install haproxy -y
 
 echo "creating the haproxy config files"
 
