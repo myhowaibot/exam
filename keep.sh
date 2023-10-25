20b9050daabdfac728b487043f3dcb6e509d838c
myhowaibot <141584790+myhowaibot@users.noreply.github.com>
2023-10-25 16:21:38 +0330

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -50,12 +50,15 @@ chmod +x /etc/keepalived/check_apiserver.sh
 
 echo "setting up the keepalived config file"
 
+# Get the number of servers to create
+read -p "Enter the number of keepalived and haproxy: " nm_servers
 # Create the haproxy configuration
-for (( i=1; i<$nm_servers; i++ ))
+for (( i=1; i<=$nm_servers; i++ ))
 do
     read -p "Enter the ip addres of ha node$i: " MIP
     configs+="        $MIP\n"
 done
+
 
 
 echo -e 'vrrp_script check_apiserver {\n  script "/etc/keepalived/check_apiserver.sh"\n  interval 1\n  timeout 1\n  fall 4\n  rise 2\n  weight -2\n}\n\nvrrp_instance VI_1 {\n    state BACKUP\n    interface $NET\n    virtual_router_id 60\n    priority 150\n    advert_int 3\n    unicast_src_ip $VIP\n    unicast_peer {\n  $configs    }\n    authentication {\n        auth_type PASS\n        auth_pass liesdfged\n    }\n    virtual_ipaddress {\n        $IP/24\n    }\n    track_script {\n        check_apiserver\n    }\n}\n'
